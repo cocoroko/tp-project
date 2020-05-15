346f5842aa5bdb08d086c8e90a7967df7a70b1e5
Esya <56122211+cocoroko@users.noreply.github.com>
2020-05-16 02:59:58 +0300

    Create humanity.cpp

---
+++ humanity/humanity.cpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "../Creator/Creators.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+class humanity {
+public:
+    humanity() = default;
+    ~humanity() = default;
+    void Addhumanity(std::string type);
+    void Info();
+
+private:
+    std::vector <*> //;
+    std::vector <*> //;
+    std::vector <*> //;
+    std::vector <*> //;
+    HCreator creator;
+};
