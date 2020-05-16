39e3d6e0b8f4d0b89fc286ab410667a5bc92df6f
Esya <56122211+cocoroko@users.noreply.github.com>
2020-05-16 03:25:46 +0300

    Update elfs.h

--- elfs/elfs.h
+++ elfs/elfs.h
@@ -1,1 +1,20 @@
-k
+#pragma once
+#include "../Creator/Creators.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+class elfs {
+public:
+    elfs() = default;
+    ~elfs() = default;
+    void Addelfs(std::string type);
+    void Info();
+
+private:
+    std::vector <ork*> ork;
+    std::vector <Elekar*> hlekar;
+    std::vector <Ecovalier*> hcovalier;
+    ECreator creator;
+};
+
