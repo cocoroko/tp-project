e31267a227db8b33b876a4ba85f686a77a9d8f50
Esya <56122211+cocoroko@users.noreply.github.com>
2020-05-16 03:34:13 +0300

    Update creators.h

--- create/creators.h
+++ create/creators.h
@@ -1,4 +1,42 @@
-#pragma once
+//
+//  makearmy.cpp
+//  qwerty
+//
+//  Created by Есения on 24/04/2020.
+//  Copyright © 2020 Есения. All rights reserved.
+//
 
+#pragma once
 #include "../Units/ConcreteUnits.h"
 
+class elfsCreator {
+public:
+    ork* Createork() {
+        return new ork;
+    }
+    Ecavalier* CreateEcavalier() {
+        return new Ecavalier;
+    }
+    Emag* CreateEmag() {
+        return new Emag;
+    }
+    Elekar* CreateElekar() {
+        return new Elekar;
+    }
+};
+
+class humanityCreator {
+public:
+    scientist* Createscientist() {
+        return new scientist;
+    }
+    Hcavalier* CreateHcavalier() {
+        return new Htcavalier;
+    }
+    Hmag* CreateHmag() {
+        return new Hmag;
+    }
+    Hlekar* CreateHlekar() {
+        return new Hlekar;
+    }
+};
