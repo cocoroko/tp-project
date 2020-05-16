5eabfeadf2ed59d728f16378215bc7928fcefaaa
Esya <56122211+cocoroko@users.noreply.github.com>
2020-05-16 03:24:15 +0300

    Update humanity.h

--- humanity/humanity.h
+++ humanity/humanity.h
@@ -1,1 +1,19 @@
-k
+#pragma once
+#include "../Creator/Creators.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+class humanity {
+public:
+    humanity() = default;
+    ~humanity() = default;
+    void Addhumanity(std::string type);
+    void Info();
+
+private:
+    std::vector <scientist*> scientist;
+    std::vector <Hlekar*> hlekar;
+    std::vector <Hcovalier*> hcovalier;
+    HCreator creator;
+};
